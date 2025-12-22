e6322b4c492e11ec7cc8123de749da319c30f392
thananpat <dodo064foik@gmail.com>
2025-12-22 10:10:53 +0700

    feat: init leader

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THANAPAT-BLOG</title>
+</head>
+<body>
+    <h1>THANAPAT-BLOG</h1>
+    <hr/>
+    <h3>ธนภัทร อัคศรี (DODO)</h3>
+    <h3>Thanapat Aksri (DODO)</h3>
+    <hr/>
+</body>
+</html>
